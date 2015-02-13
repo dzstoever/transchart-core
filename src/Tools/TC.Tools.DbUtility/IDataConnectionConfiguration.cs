903e0a946cdba9b7a3edd96ddee6ecb8e0825d94
dzstoever <dan.stoever@live.com>
2015-02-13 04:41:16 -0500

    refactor

---
+++ src/Tools/TC.Tools.DbUtility/IDataConnectionConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace TC.Tools.DbUtility
+{
+	public interface IDataConnectionConfiguration
+	{
+		string GetSelectedSource();
+		void SaveSelectedSource(string provider);
+
+		string GetSelectedProvider();
+		void SaveSelectedProvider(string provider);
+	}
+}
